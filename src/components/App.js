4edf4c556e0add991a8c90f44e3ca719010fe0aa
Lionel T <elrumordelaluz@hotmail.com>
2016-06-22 14:23:51 +0200

    Remove commented lines

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import appStyles from './App.css';
 class App extends Component {
   render () {
     return (
-      // <div className={appStyles.app}>My App</div>
       <div className="app">My App</div>
     );
   }
