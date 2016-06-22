5d566410200a615238e17c484f0febec873baaaa
Lionel T <elrumordelaluz@hotmail.com>
2016-06-22 14:20:01 +0200

    Initial

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import appStyles from './App.css';
+
+class App extends Component {
+  render () {
+    return (
+      // <div className={appStyles.app}>My App</div>
+      <div className="app">My App</div>
+    );
+  }
+}
+
+export default App;
