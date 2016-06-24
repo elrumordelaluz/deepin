63655e0c4b256a6f09192bfeff402616c3850a9a
Lionel T <elrumordelaluz@hotmail.com>
2016-06-24 19:06:43 +0200

    Update test

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,76 @@
 import React, { Component } from 'react';
 import appStyles from './App.css';
-
+import ColorPicker from 'coloreact';
 import Element from './shared/Element';
 import { strokedIcon, filledIcon } from '../icons';
-
 import { deepInColor, deepInStroke } from '../lib';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      color1: '#f00',
+      color2: '#f0f',
+      iconStroke: strokedIcon,
+      iconFill: filledIcon,
+      showSecondary: false,
+    }
+    this.handleToggleSecondary = this.handleToggleSecondary.bind(this);
+    this.handleChangeColor1 = this.handleChangeColor1.bind(this);
+    this.handleChangeColor2 = this.handleChangeColor2.bind(this);
+  }
+
+  handleToggleSecondary () {
+    this.setState({
+      showSecondary: !this.state.showSecondary
+    })
+  }
+
+  handleChangeColor1 (color) {
+    this.setState({
+      color1: color.hex,
+      iconFill: deepInColor(this.state.iconFill, color.hex, this.state.showSecondary ? 'color-1' : 'all')
+    })
+  }
+
+  handleChangeColor2 (color) {
+    this.setState({
+      color2: color.hex,
+      iconFill: deepInColor(this.state.iconFill, color.hex, this.state.showSecondary ? 'color-2' : 'all')
+    })
+  }
+
   render () {
-    const strokeChanged = deepInColor(strokedIcon, 'red');
-    console.log(deepInStroke(strokedIcon, {
-      strokeWidth: 1,
-      stroke: 'red'
-    }))
-    const fillChanged = deepInColor(filledIcon, 'green', 'color-2')
+    const strokeChanged = deepInColor(
+      deepInStroke(strokedIcon, { strokeWidth: 3 }),
+      'blue');
+    const fillChanged = deepInColor(filledIcon, 'green', 'accent-color')
     return (
       <div className="app">
-
-        <div className="icon">
-          <Element obj={strokeChanged.paths} />
+        <div className="controls">
+          <button onClick={this.handleToggleSecondary}>Show secondary</button>
+          <div className="pickers">
+            <div className="colorpicker">
+              <ColorPicker
+                opacity={false}
+                color={this.state.color1}
+                onChange={this.handleChangeColor1} />
+            </div>
+            <div className="colorpicker" style={{ opacity: this.state.showSecondary ? 1 : 0  }}>
+              <ColorPicker
+                opacity={false}
+                color={this.state.color2}
+                onChange={this.handleChangeColor2} />
+            </div>
+          </div>
         </div>
 
         <div className="icon">
-          <Element obj={fillChanged.paths} />
+          <Element obj={this.state.iconStroke.paths} />
+        </div>
+
+        <div className="icon">
+          <Element obj={this.state.iconFill.paths} />
         </div>
 
       </div>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,16 +1,16 @@
 import cloneDeepWith from 'lodash/cloneDeepWith';
 import hasIn from 'lodash/hasIn';
+import has from 'lodash/has';
 
-const deepColor = (icon, color, isStroke = false, layer = 'all') => {
+const deepColor = (icon, color, isStroke = false, layer = 'all', sel = 'id') => {
   const pathType = isStroke ? 'stroke' : 'fill'
   const modifier = layer !== 'all' ? layer : null;
-  const sel = 'id';
 
   const customizer = (val, key, obj, stack) => {
     const filter = () => key === pathType && val !== 'none' ? color : undefined
 
     if (layer !== 'all') {
-      if (obj && obj.hasOwnProperty(sel) && obj[sel] === layer) {
+      if (obj && has(obj, sel) && obj[sel] === layer) {
         return filter()
       }
     } else {
@@ -25,7 +25,7 @@ export const deepInColor = (icon, color, layer) => {
   return deepColor(icon, color, icon.style === 'stroke', layer)
 }
 
-const deepStroke = (icon, obj) => {
+export const deepInStroke = (icon, obj) => {
   const customizer = (val, key) => {
     if (hasIn(obj, key)) {
       return obj[key]
@@ -33,7 +33,3 @@ const deepStroke = (icon, obj) => {
   }
   return cloneDeepWith(icon, customizer)
 }
-
-export const deepInStroke = (icon, obj) => {
-  return deepStroke(icon, obj)
-};
