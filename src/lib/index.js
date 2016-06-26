deadd7be4da4652b9d39bae826c7148f10a78c23
Lionel T <elrumordelaluz@hotmail.com>
2016-06-26 11:16:08 +0200

    Remove unused log

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,7 +6,6 @@ const deepColor = (icon, color, isStroke = false, layer = 'all', sel = 'id') => 
   const pathType = isStroke ? 'stroke' : 'fill'
   const modifier = layer !== 'all' ? layer : null;
   const regex = /^\!/;
-  // console.log(regex.test(layer))
 
   const customizer = (val, key, obj, stack) => {
     const filter = () => key === pathType && val !== 'none' ? color : undefined
